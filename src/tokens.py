b06d0377acbba96bacf3db43e45597051c027a75
sphennings <sphenning@gmail.com>
2023-01-05 01:48:32 -0500

    Fix pylint and flake8 errors for tokens.py

--- src/tokens.py
+++ src/tokens.py
@@ -1,12 +1,22 @@
+"""Tokens
+
+This module defines the tokens datatype and how to scan source code
+into tokens
+
+"""
+
 from dataclasses import dataclass
 from enum import Enum, auto
 
 
 class UnexpectedCharacterException(Exception):
-    pass
+    """An unexpected character was encounterd while scanning the
+    soruce code"""
 
 
 class TokenType(Enum):
+    """The types of tokens in pecan lisp"""
+
     LPAREN = auto()
     RPAREN = auto()
     SYMBOL = auto()
@@ -16,24 +26,29 @@ class TokenType(Enum):
 
 @dataclass
 class Token:
+    """Dataclass for tokens in pecan lisp"""
+
     type: TokenType
     lexeme: str
     literal: float | str | None
     line: int
 
     def __repr__(self) -> str:
-        repr = f"{self.type.name} line: {self.line}, lexeme: {self.lexeme}"
+        representation = f"{self.type.name} line: {self.line}, lexeme: {self.lexeme}"
         if self.literal is not None:
-            repr = repr + f" literal: {self.literal}"
-        return repr
+            representation = representation + f" literal: {self.literal}"
+        return representation
 
 
 def scan(src: str) -> list[Token]:
+    """Takes a source string and produces a list of tokens"""
     scanner = Scanner(src)
     return scanner.scan()
 
 
 class Scanner:
+    """The class that actually does the scanning"""
+
     def __init__(self, source: str):
         self.source = source
         self.tokens: list[Token] = []
@@ -42,6 +57,7 @@ class Scanner:
         self.line = 1
 
     def scan(self):
+        """Start scanning the source"""
         while not self.is_at_end():
             # We're at the beginning of a lexeme
             self.start = self.current
@@ -51,21 +67,24 @@ class Scanner:
         return self.tokens
 
     def scan_token(self):
-        c = self.advance()
-        if c == "(":
+        """Consumes the next character and creates a token if necessary"""
+        # TODO: Use structural pattern matching instead
+        char = self.advance()
+        if char == "(":
             self.add_token(TokenType.LPAREN)
-        elif c == ")":
+        elif char == ")":
             self.add_token(TokenType.RPAREN)
-        elif c in " \r\t":
+        elif char in " \r\t":
             pass
-        elif c == "\n":
+        elif char == "\n":
             self.line += 1
-        elif c.isdigit():
+        elif char.isdigit():
             self.number()
         else:
             self.symbol()
 
     def number(self):
+        """Consumes numeric characters to create a single number token"""
         while self.peek().isdigit():
             self.advance()
 
@@ -80,30 +99,36 @@ class Scanner:
         self.add_token(TokenType.NUMBER, float(self.source[self.start : self.current]))
 
     def symbol(self):
+        """Consumes characters to creats a single symbol token"""
         while self.peek() not in ") \n" and not self.is_at_end():
             self.advance()
 
         self.add_token(TokenType.SYMBOL, self.source[self.start : self.current])
 
     def is_at_end(self):
+        """Checks to see if we're at the end of the source or not"""
         return self.current >= len(self.source)
 
     def advance(self) -> str:
-        c = self.source[self.current]
+        """Returns the current character, then advances one character"""
+        char = self.source[self.current]
         self.current += 1
-        return c
+        return char
 
     def peek(self) -> str:
+        """Returns the current character without advancing"""
         if self.is_at_end():
             return "\0"
 
         return self.source[self.current]
 
     def peek_next(self) -> str:
+        """Returns the next character without advancing"""
         if (self.current + 1) >= len(self.source):
             return "\0"
         return self.source[self.current + 1]
 
     def add_token(self, token_type: TokenType, literal: str | float | None = None):
+        """Creates a  token of the appropriate type and adds it to the list of tokens"""
         text = self.source[self.start : self.current]
         self.tokens.append(Token(token_type, text, literal, self.line))
